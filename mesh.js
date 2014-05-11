43adce50d68e4a918946c40513bd153c3428aab9
Will Pearson <pearswj@gmail.com>
2014-05-11 16:03:33 +0100

    add halfedge section + many mouseovers
    * hover over face to see adjacent halfedges
    * hover over halfedge to see adjacent halfedges

--- mesh.js
+++ mesh.js
@@ -56,6 +56,21 @@ function getPairHalfedge(i) {
 //// CIRCULATORS
 var circulatorlimit = 999;
 
+function getFaceHalfedges(halfedge)
+{
+  var halfedges = [];
+  //var halfedge = mesh.faces[f].halfedge;
+  var h = halfedge;
+  var count = 0;
+  do {
+    halfedges.push(h);
+    h = mesh.halfedges[h].next;
+    if (h < 0) { throw "Unset index, cannot continue."; }
+    if (count++ > circulatorlimit) { throw "Runaway face circulator."; }
+  } while (h != halfedge);
+  return halfedges;
+}
+
 // get vertices for a given face (specified by one its adjacent halfedges)
 function getFaceVertices(halfedge)
 {
@@ -190,6 +205,72 @@ for (var i = 0; i < faces.length; i++) {
   addFace(faces[i]);
 }
 
+function getEdgeVector(i) {
+  var d = mesh.halfedges[i];
+  var x1 = mesh.vertices[d.start].x;
+  var x2 = mesh.vertices[mesh.halfedges[getPairHalfedge(i)].start].x;
+  var y1 = mesh.vertices[d.start].y;
+  var y2 = mesh.vertices[mesh.halfedges[getPairHalfedge(i)].start].y;
+  var x = x2 - x1;
+  var y = y2 - y1;
+  var inverseLength = 1.0 / Math.sqrt(x * x + y * y);
+  return {
+    x: x * inverseLength,
+    y: y * inverseLength
+  };
+}
+
+// rotate a vector (radians)
+function rotateVector(vector, theta) {
+  cs = Math.cos(theta);
+  sn = Math.sin(theta);
+  return {
+    x: vector.x * cs - vector.y * sn,
+    y: vector.x * sn + vector.y * cs
+  };
+}
+
+// create edge lines
+function createEdgeLines() {
+  lines = []
+  var n = mesh.halfedges.length;
+  for (var i = 0; i < n; i += 2) {
+    var d = mesh.halfedges[i];
+    var x1 = mesh.vertices[d.start].x;
+    var x2 = mesh.vertices[mesh.halfedges[getPairHalfedge(i)].start].x;
+    var y1 = mesh.vertices[d.start].y;
+    var y2 = mesh.vertices[mesh.halfedges[getPairHalfedge(i)].start].y;
+    lines.push({
+      x1: x1,
+      x2: x2,
+      y1: y1,
+      y2: y2
+    });
+  }
+  return lines;
+}
+
+function createOffsetEdgeLines(offset) {
+  lines = []
+  var n = mesh.halfedges.length;
+  for (var i = 0; i < n; i++) {
+    var d = mesh.halfedges[i];
+    var x1 = mesh.vertices[d.start].x;
+    var x2 = mesh.vertices[mesh.halfedges[getPairHalfedge(i)].start].x;
+    var y1 = mesh.vertices[d.start].y;
+    var y2 = mesh.vertices[mesh.halfedges[getPairHalfedge(i)].start].y;
+    var eVec = getEdgeVector(i);
+    var oVec = rotateVector(eVec, Math.PI * -0.5);
+    lines.push({
+      x1: x1 + offset * oVec.x,
+      x2: x2 + offset * oVec.x,
+      y1: y1 + offset * oVec.y,
+      y2: y2 + offset * oVec.y
+    });
+  }
+  return lines;
+}
+
 //--------------------------- END:   HALFEDGE MESH ---------------------------//
 
 // create SVG element: vertices
@@ -197,24 +278,6 @@ var svg = d3.select("#points .sectionGraphic")
   .append("svg")
   .attr("width", w)
   .attr("height", h);
-  
-/*
-svg.append("line")
-  .attr("x1", 0)
-  .attr("x2", 40)
-  .attr("y1", h)
-  .attr("y2", h)
-  .attr("stroke-width", 2)
-  .attr("stroke", "black");
-  
-svg.append("line")
-  .attr("x1", 0)
-  .attr("x2", 0)
-  .attr("y1", h)
-  .attr("y2", h - 40)
-  .attr("stroke-width", 2)
-  .attr("stroke", "black");
-*/
 
 var c = svg.selectAll("circle")
   .data(mesh.vertices)
@@ -281,46 +344,149 @@ svg2.selectAll("polygon")
   .append("polygon")
   .attr("points", function(d) {
   	return getFaceVertices(d.halfedge).map(function(e) {
-  		return vertices[e].join(",");
+  		return mesh.vertices[e].x + "," + mesh.vertices[e].y;
   	}).join(" ");
   })
-  .attr("stroke", "black")
-  .attr("stroke-width", 2)
-  .attr("fill", "#DEF9EE");
+  //.attr("stroke", "black")
+  //.attr("stroke-width", .5)
+  //.attr("stroke-dasharray", "5,5")
+  .attr("fill", "#DEF9EE")
+  .attr("face", function(d, i) {
+    return i;
+  })
+  .on('mouseover', function(d) {
+    var h = mesh.faces[this.getAttribute('face')].halfedge;
+    //d3.selectAll(".halfedge-" + h).style({'stroke-width':3});
+    var halfedges = getFaceHalfedges(h);
+    for (var i = 0; i < halfedges.length; i++) {
+      var h = halfedges[i];
+      d3.selectAll(".halfedge-" + halfedges[i]).style({'stroke-width':3});
+    }
+    
+  })
+  .on('mouseout', function(d) {
+    var h = mesh.faces[this.getAttribute('face')].halfedge;
+    //d3.selectAll(".halfedge-" + h).style({'stroke-width':1});
+    var halfedges = getFaceHalfedges(h);
+    for (var i = 0; i < halfedges.length; i++) {
+      d3.selectAll(".halfedge-" + halfedges[i]).style({'stroke-width':1});
+    }
+  });
 
-  var c2 = svg2.selectAll("circle")
-    .data(mesh.vertices)
-    .enter()
-  
-    c2.append("circle")
-    .attr("cx", function(d) {
-    	return d.x;
-    })
-    .attr("cy", function(d) {
-    	return d.y;
-    })
-    .attr("r", 4)
-    .attr("class", function(d, i) {
-      return "vertex-" + i;
-    })
-    .on('mouseover', function(d) {
-        d3.selectAll("." + this.getAttribute('class') + "-halo").style({visibility:'visible'});
-    })
-    .on('mouseout', function(d) {
-        d3.selectAll("." + this.getAttribute('class') + "-halo").style({visibility:'hidden'});
-    });
+var polygons = [];
+for (var i = 0; i < faces.length; i++) {
+  polygons.push(d3.geom.polygon(faces[i].map(function(d) {
+    return vertices[d];
+  })));
+}
+
+svg2.selectAll("text")
+  .data(polygons)
+  .enter()
+  .append("text")
+  .text(function(d, i) {
+  	return i;
+  })
+  .attr("x", function(d) {
+  	return d.centroid()[0];
+  })
+  .attr("y", function(d) {
+  	return d.centroid()[1];
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "11px") 
+
+
+svg2.selectAll("line")
+  .data(createEdgeLines())
+  .enter()
+  .append("line")
+  .attr("x1", function(d) {
+    return d.x1;
+  })
+  .attr("x2", function(d) {
+    return d.x2;
+  })
+  .attr("y1", function(d) {
+    return d.y1;
+  })
+  .attr("y2", function(d) {
+    return d.y2;
+  })
+  .attr("stroke", "black")
+  .attr("stroke-width", .5)
+  .attr("stroke-dasharray", "5,5")
+
+var c2 = svg2.selectAll("circle")
+  .data(mesh.vertices)
+  .enter()
 
   c2.append("circle")
-    .attr("cx", function(d) {
-    	return d.x;
-    })
-    .attr("cy", function(d) {
-    	return d.y;
-    })
-    .attr("r", 10)
-    .attr("fill", "none")
-    .attr("stroke", "black")
-    .attr("class", function(d, i) {
-      return "vertex-" + i + "-halo";
-    })
-    .attr("visibility", "hidden");
+  .attr("cx", function(d) {
+  	return d.x;
+  })
+  .attr("cy", function(d) {
+  	return d.y;
+  })
+  .attr("r", 2)
+  .attr("class", function(d, i) {
+    return "vertex-" + i;
+  })
+  .on('mouseover', function(d) {
+      d3.selectAll("." + this.getAttribute('class') + "-halo").style({visibility:'visible'});
+  })
+  .on('mouseout', function(d) {
+      d3.selectAll("." + this.getAttribute('class') + "-halo").style({visibility:'hidden'});
+  });
+
+c2.append("circle")
+  .attr("cx", function(d) {
+  	return d.x;
+  })
+  .attr("cy", function(d) {
+  	return d.y;
+  })
+  .attr("r", 10)
+  .attr("fill", "none")
+  .attr("stroke", "black")
+  .attr("class", function(d, i) {
+    return "vertex-" + i + "-halo";
+  })
+  .attr("visibility", "hidden");
+
+// create another SVG element: faces
+var svg3 = d3.select("#halfedges .sectionGraphic")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
+    
+svg3.selectAll("line")
+  .data(createOffsetEdgeLines(3.0))
+  .enter()
+  .append("line")
+  .attr("x1", function(d) {
+    return d.x1;
+  })
+  .attr("x2", function(d) {
+    return d.x2;
+  })
+  .attr("y1", function(d) {
+    return d.y1;
+  })
+  .attr("y2", function(d) {
+    return d.y2;
+  })
+  .attr("stroke-width", 1)
+  .attr("stroke", "black")
+  .attr("class", function(d, i) {
+    return "halfedge-" + i;
+  })
+  .attr("halfedge", function(d, i) {
+    return i;
+  })
+  .on('mouseover', function(d) {
+      d3.selectAll(".halfedge-" + mesh.halfedges[this.getAttribute('halfedge')].next).style({'stroke-width':3});
+  })
+  .on('mouseout', function(d) {
+      d3.selectAll(".halfedge-" + mesh.halfedges[this.getAttribute('halfedge')].next).style({'stroke-width':1});
+  });
